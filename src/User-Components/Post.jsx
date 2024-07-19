b988b7f7f03a8e2407fce463befa9727520be61f
ajikyasharma <ajikyasharma24@gamil.com>
2024-07-19 17:55:21 +0530

    fix: fixed video resolution

--- src/User-Components/Post.jsx
+++ src/User-Components/Post.jsx
@@ -76,16 +76,16 @@ function Post() {
       },[])
 
       if (loading) {
-        return <div className='h-screen flex justify-center items-center'><CircularProgress  /></div>; // Show a loading message or spinner while fetching data
+        return <div className='min-h-screen flex justify-center items-center'><CircularProgress  /></div>; // Show a loading message or spinner while fetching data
       }
 
 
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center font-Poppins sm:pt-[80px] ">
+    <div className="min-h-screen flex flex-col items-center justify-center font-Poppins sm:pt-[80px] pb-[100px]">
     <main className="container mx-auto p-4 flex flex-col lg:flex-row bg-white rounded-lg md:shadow-lg">
       {/* Left side content */}
       <div className="flex-1 lg:mr-4 mb-4 lg:mb-0">
-        <div className="w-full aspect-w-1 aspect-h-1 flex items-center justify-center">
+        <div className="w-full  flex items-center justify-center">
           {/* <img
             src={post && post.post_photo ? post.post_photo : ''}
             alt="Post"
@@ -94,10 +94,15 @@ function Post() {
 
 {post.post_file && (
            post.file_type && post.file_type === 'video' ? (
-              <video controls className="w-96 h-96 sm:w-[500px] sm:h-[500px]  md:w-[600px] md:h-[600px] lg:w-[700px] lg:h-[700px] object-cover">
-                <source src={post.post_file} type="video/mp4" />
-                Your browser does not support the video tag.
-              </video>
+            <video 
+            controls 
+            autoPlay
+            loop
+            muted
+            className="w-[325px] h-[500px] sm:w-[500px] sm:h-[600px]  md:w-[550px] md:h-[675px]  object-cover">
+              <source src={post.post_file} type="video/mp4" />
+              Your browser does not support the video tag.
+            </video>
             ) : (
                                <img
                   src={post && post.post_file ? post.post_file : ''}
