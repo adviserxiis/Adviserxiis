d2aed66ed9d53127acf1577736e839fd1c2be9f2
ajikyasharma <ajikyasharma24@gamil.com>
2024-07-29 15:15:46 +0530

    created views count feature

--- src/User-Components/CustomVideo.jsx
+++ src/User-Components/CustomVideo.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import PlayArrowIcon from '@mui/icons-material/PlayArrow';
 import PauseIcon from '@mui/icons-material/Pause';
 import VolumeOffIcon from '@mui/icons-material/VolumeOff';
@@ -9,9 +9,17 @@ import ShareIcon from '@mui/icons-material/Share';
 import User from '../assets/User.png'
 import ShareDialog from './ShareDialog';
 import { useNavigate } from 'react-router-dom';
+import { child, get, getDatabase, ref, remove, set, update } from "firebase/database";
+import { app } from "../firebase";
+import { getAuth } from 'firebase/auth';
+import RemoveRedEyeOutlinedIcon from '@mui/icons-material/RemoveRedEyeOutlined';
 
-const CustomVideo = ({data, addLike, removeLike}) => {
+const CustomVideo = ({ data, addLike, removeLike }) => {
   const videoRef = useRef(null);
+
+  const database = getDatabase(app);
+  const auth = getAuth();
+
   const [isPlaying, setIsPlaying] = useState(true);
   const [isMuted, setIsMuted] = useState(true);
   const [showIcon, setShowIcon] = useState(false);
@@ -19,9 +27,55 @@ const CustomVideo = ({data, addLike, removeLike}) => {
   const [isLiked, setIsLiked] = useState(false);
   const [shareDialogOpen, setShareDialogOpen] = useState(false);
   const [shareURL, setShareURL] = useState('')
+  const [hasViewed, setHasViewed] = useState(false);
 
   const userid = JSON.parse(localStorage.getItem('userid'));
   const navigate = useNavigate()
+
+  useEffect(() => {
+    const handleIntersection = (entries) => {
+      const entry = entries[0];
+      if (entry.isIntersecting && !hasViewed) {
+        setHasViewed(true);
+        incrementViewCount(data.id);
+      }
+    };
+
+    const observer = new IntersectionObserver(handleIntersection, {
+      threshold: 0.5, // Adjust this value as needed
+    });
+
+    if (videoRef.current) {
+      observer.observe(videoRef.current);
+    }
+
+    return () => {
+      if (videoRef.current) {
+        observer.unobserve(videoRef.current);
+      }
+    };
+  }, [hasViewed, data.id]);
+
+  const incrementViewCount = async (postId) => {
+    try {
+      const postRef = ref(database, 'advisers_posts/' + postId);
+      const postDataSnapshot = await get(postRef);
+      const postData = postDataSnapshot.val();
+
+      const currentViews = postData.views || 0;
+      const updatedViews = currentViews + 1;
+
+      await update(postRef, { views: updatedViews });
+      // console.log(`View count for post ${postId} updated to ${updatedViews}`);
+    } catch (error) {
+      console.error('Error updating view count:', error);
+    }
+  };
+
+  // const incrementViewCount = (postId) => {
+  //   // Replace this with your actual API call or logic to increment view count
+  //   console.log(`Increment view count for post: ${postId}`);
+  // };
 
   const handleVideoClick = () => {
     if (videoRef.current.paused) {
@@ -61,16 +115,16 @@ const CustomVideo = ({data, addLike, removeLike}) => {
     setIsLiked(!isLiked);
   };
 
-  const handleClickOnProfile = ( adviserName, adviserId) =>{
-      
+  const handleClickOnProfile = (adviserName, adviserId) => {
+
     navigate(`/category/${adviserName}`, {
       state: {
         adviserid: adviserId,
         advisername: adviserName
       }
     })
-  
-     }
+
+  }
 
 
 
@@ -92,25 +146,30 @@ const CustomVideo = ({data, addLike, removeLike}) => {
           {!isPlaying ? <PlayArrowIcon /> : <PauseIcon />}
         </div>
       )}
+
+<div className="absolute flex flex-col justify-center items-center top-20 sm:top-16 right-4 text-white text-3xl md:text-4xl  cursor-pointer z-30" onClick={handleMuteClick}>
+        <RemoveRedEyeOutlinedIcon fontSize='inherit' />
+        <p className='text-white text-lg md:text-xl font-semibold'>{data?.data && data?.data?.views ? data.data.views : 0}</p>
+      </div>
       <div className="absolute bottom-44 sm:bottom-36 right-4 text-white text-3xl md:text-4xl  cursor-pointer z-30" onClick={handleMuteClick}>
         {isMuted ? <VolumeOffIcon fontSize="inherit" /> : <VolumeUpIcon fontSize="inherit" />}
       </div>
       <div className="absolute bottom-56 sm:bottom-48 right-4 flex flex-col  items-center space-y-2  z-30">
-        <div  className=" flex flex-col items-center cursor-pointer">
+        <div className=" flex flex-col items-center cursor-pointer">
           {/* {isLiked ? <FavoriteIcon className="text-red-500" fontSize='large' /> : <FavoriteBorderIcon className="text-white" fontSize='large' />} */}
           {data.data && data.data.likes && data.data.likes.includes(userid) ? <div className="cursor-pointer text-3xl md:text-3xl lg:text-4xl ">
-                    <FavoriteIcon className='text-red-500' fontSize="inherit" onClick={() => removeLike(data?.id)} />
-                  </div> : <div className="cursor-pointer text-3xl md:text-4xl ">
-                    <FavoriteBorderIcon className='text-white' fontSize="inherit" onClick={() => addLike(data?.id)} />
-                  </div>}
-                  <p className='text-white text-lg md:text-xl font-semibold'>{data?.data && data?.data?.likes ? data.data.likes.length : 0}</p>
+            <FavoriteIcon className='text-red-500' fontSize="inherit" onClick={() => removeLike(data?.id)} />
+          </div> : <div className="cursor-pointer text-3xl md:text-4xl ">
+            <FavoriteBorderIcon className='text-white' fontSize="inherit" onClick={() => addLike(data?.id)} />
+          </div>}
+          <p className='text-white text-lg md:text-xl font-semibold'>{data?.data && data?.data?.likes ? data.data.likes.length : 0}</p>
         </div>
-        <div onClick={()=>handleShareClick(data?.id)} className="cursor-pointer text-white text-3xl md:text-4xl ">
-          <ShareIcon fontSize='inherit'/>
+        <div onClick={() => handleShareClick(data?.id)} className="cursor-pointer text-white text-3xl md:text-4xl ">
+          <ShareIcon fontSize='inherit' />
         </div>
       </div>
       <div className="absolute bottom-32 sm:bottom-20 left-0 w-full p-4  text-white">
-        <div className="flex items-center mb-2 cursor-pointer" onClick={()=>handleClickOnProfile(data?.adviser?.data?.username, data?.adviser?.id)}>
+        <div className="flex items-center mb-2 cursor-pointer" onClick={() => handleClickOnProfile(data?.adviser?.data?.username, data?.adviser?.id)}>
           <img src={data?.adviser?.data?.profile_photo || User} alt="Profile" className="w-10 h-10 rounded-full mr-2" />
           <span className="font-bold">{data?.adviser?.data?.username || ''}</span>
         </div>
@@ -122,10 +181,10 @@ const CustomVideo = ({data, addLike, removeLike}) => {
         </div>
       </div>
       <ShareDialog
-                  open={shareDialogOpen}
-                  handleClose={handleShareDialogClose}
-                  url={shareURL}
-                />
+        open={shareDialogOpen}
+        handleClose={handleShareDialogClose}
+        url={shareURL}
+      />
     </div>
   );
 };
